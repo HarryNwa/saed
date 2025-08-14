7b43a545d63274c3144d41e6c0eebff612920df5
Harry Nwa <harrynwa90@gmail.com>
2025-08-14 19:44:48 +0100

    first push

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -100,10 +100,3 @@ const Navigation = () => {
 export default Navigation;
 
 
-echo "# saed" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/HarryNwa/saed.git
-git push -u origin main
